4c4024fc3e6012f5cf7ffcdb2ebb6bd871e98ae2
Craig Wendorf <cwendorf@uwsp.edu>
2025-08-04 06:54:23 -0500

    Update spToolsExamples.R

--- scripts/spToolsExamples.R
+++ scripts/spToolsExamples.R
@@ -15,6 +15,12 @@ ci.mean2(alpha = .05, 11.000, 8.000, 2.211, 1.414, 10, 10)[1, c(1,6,7)] -> Compa
 Results <- rbind(Level1, Level2, Comparison)
 rownames(Results) <- c("Level1", "Level2", "Comparison")
 
+ci.mean(alpha = .05, m = 8.000, sd = 1.414, n = 10) |> intervals() -> Level1
+ci.mean(alpha = .05, m = 11.000, sd = 2.2111, n = 10) |> intervals() -> Level2
+ci.mean2(alpha = .05, 11.000, 8.000, 2.211, 1.414, 10, 10) |> intervals() |> rows(1) -> Comparison
+Results <- rbind(Level1, Level2, Comparison)
+rownames(Results) <- c("Level1", "Level2", "Comparison")
+
 ### Results
 
 Results
@@ -23,8 +29,8 @@ Results |> plot_comp(title = "Comparison Plot", values = TRUE)
 
 ### Input Summary
 
-ci.mean.vec(alpha = .05, m = c(8.000, 11.000), sd = c(1.414, 2.211), n = c(10, 10)) |> sp.columns() -> Levels
-ci.mean2.vec(alpha = .05, m = c(11.000, 8.000), sd = c(2.211, 1.414), n = c(10, 10)) |> sp.columns() |> sp.rows(1) -> Comparison
+ci.mean.vec(alpha = .05, m = c(8.000, 11.000), sd = c(1.414, 2.211), n = c(10, 10)) -> Levels
+ci.mean2.vec(alpha = .05, m = c(11.000, 8.000), sd = c(2.211, 1.414), n = c(10, 10)) |> columns() |> rows(1) -> Comparison
 Results <- rbind(Levels, Comparison)
 rownames(Results) <- c("Level1", "Level2", "Comparison")
 
